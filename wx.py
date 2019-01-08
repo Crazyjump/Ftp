942f752438fd87f40a26c682487ebe1a95043648
Your Name <crazyjump@163.com>
2019-01-08 13:08:40 +0800

    合并2

--- wx.py
+++ wx.py
@@ -1,7 +1,11 @@
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 dddddddddddddddd
 xxxxxxxxxxxxx
 
+=======
+xxxxxxxxxxxxx
+>>>>>>> Stashed changes
 =======
 xxxxxxxxxxxxx
 >>>>>>> Stashed changes
